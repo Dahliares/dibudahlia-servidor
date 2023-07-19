027fe8496ce638f1f36d8baf2cbce85da302baac
Saraoz <roseofpain1@gmail.com>
2023-07-19 12:53:44 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -162,13 +162,7 @@ app.post('/resetpass', (req, res) => {
 
 });
 
-// Hacer que node sirva los archivos de nuestro app React
-app.use(express.static(path.resolve(__dirname, '../dibudahlia-servidor/build')));
 
-// Todas las peticiones GET que no hayamos manejado en las líneas anteriores retornaran nuestro app React
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../dibudahlia-servidor/build', 'index.html'));
-  });
 
 
 
